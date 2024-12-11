63ac7ae80ce436a54fe36368102e21bf1583950b
xomix1802 <xomix1802@gmail.com>
2024-12-11 15:57:33 +0000

    feat: update data table styles

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,21 @@
   <v-app>
     <v-main>
       <Appbar />
-      <v-card>
-        <v-tabs v-model="tab" color="deep-purple-accent-4">
-          <v-tab :value="1">Pinned</v-tab>
-          <v-tab :value="2">Transit Notes</v-tab>
-          <v-tab :value="3">Drafts</v-tab>
+      <v-card class="pa-4">
+        <!-- TODO: Extract to a component -->
+        <v-tabs v-model="tab" slider-color="transparent" class="ga-3">
+          <v-tab :value="1" rounded="t-lg" border="sm" variant="flat" color="grey-darken-3" class="mx-1">
+            Pinned (4)
+            <template v-slot:append>
+              <v-btn icon="mdi-close" variant="text" size="small" density="compact"></v-btn>
+            </template>
+            <template v-slot:prepend>
+              <v-btn icon="mdi-pin" color="primary" variant="text" size="small" density="compact"></v-btn>
+            </template>
+          </v-tab>
+          <v-tab :value="2" rounded="t-lg" border="sm" variant="flat" color="grey-darken-3" class="mx-1">Transit
+            Notes</v-tab>
+          <v-tab :value="3" rounded="t-lg" border="sm" variant="flat" color="grey-darken-3" class="mx-1">Drafts</v-tab>
         </v-tabs>
         <v-tabs-window v-model="tab">
           <v-tabs-window-item :value="1">
@@ -14,21 +24,21 @@
           </v-tabs-window-item>
           <v-tabs-window-item :value="2">
             <p v-if="error" style="white-space: pre-line;">{{ error }}</p>
-            <v-checkbox label="Advanced search"> Need to be rounded</v-checkbox>
+            <v-sheet class="d-flex align-center">
+              Advanced search <v-checkbox :hide-details="true" />
+            </v-sheet>
             <v-form>
               <v-row>
                 <v-col cols="6" md="2">
-                  <v-text-field label="Shipping Note Reference" placeholder="Enter value" variant="underlined"
-                    required></v-text-field>
+                  <v-text-field v-model="shippingNoteReference" label="Shipping Note Reference"
+                    placeholder="Enter value" variant="underlined" required></v-text-field>
                 </v-col>
-
                 <v-col cols="6" md="2">
                   <v-select variant="underlined" item-title="label" item-value="value" chips label="Departure Office"
                     :items="departureOfficeOptions" multiple
                     :placeholder="`${departureOfficeOptions.length} items available`">
                   </v-select>
                 </v-col>
-
                 <v-col cols="6" md="2">
                   <v-select variant="underlined" item-title="label" clearable item-value="value" chips
                     label="Destination Office" :items="destinationOfficeOptions" multiple></v-select>
@@ -44,18 +54,49 @@
                 </v-col>
               </v-row>
             </v-form>
-            <v-data-table :loading="loading" :items="items" :items-per-page="2" :headers="headers" fixed-header
-              show-select item-key="id">
+            <v-data-table :search="shippingNoteReference" filter-keys="reference" :loading="loading" :items="items"
+              :items-per-page="pagination.size" :headers="headers" fixed-header item-key="id" hide-default-footer>
               <template v-slot:top>
                 <div class="d-flex align-center justify-space-between">
                   <v-btn prepend-icon="mdi-plus" variant="tonal" color="primary">
                     Create new shipping note
                   </v-btn>
-                  <v-data-table-footer items-per-page-text="$vuetify.dataTable.itemsPerPageText" />
+                  <v-sheet class="d-flex align-center">
+                    Size <v-select variant="solo" :hide-details="true" :items="[10, 20, 50, 100]" width="80"
+                      :model-value="10" />
+                    <span>
+                      {{ pagination.page + 1 }} - {{ pagination.page * pagination.size + pagination.size }} of {{
+                        pagination.totalElements }}
+                    </span>
+                    <v-pagination density="compact" :length="pagination.totalPages" :total-visible="6"
+                      active-color="primary" :page="pagination.page"></v-pagination>
+                    <v-btn icon="mdi-chevron-double-right" variant="flat" />
+                    <v-btn icon="mdi-order-bool-descending-variant" variant="flat" />
+                  </v-sheet>
+                  <!-- <v-data-table-footer items-per-page-text="$vuetify.dataTable.itemsPerPageText" /> -->
                 </div>
               </template>
+              <template v-slot:item.actions>
+                <v-sheet class="d-flex">
+                  <v-btn icon="mdi-dots-horizontal" size="sm" slim></v-btn>
+                  <v-btn icon="mdi-bookmark-outline" size="sm" slim></v-btn>
+                </v-sheet>
+              </template>
+              <template v-slot:item.status="{ value }">
+                <v-chip :color="getColor(value)">
+                  {{ value }}
+                </v-chip>
+              </template>
+              <template v-slot:item.submittedDate="{ value }">
+                {{ formatDate(value) }}
+              </template>
+              <template v-slot:item.departureDateTime="{ value }">
+                {{ formatDate(value) }}
+              </template>
+              <template v-slot:item.estimatedArrivalDate="{ value }">
+                {{ formatDate(value) }}
+              </template>
             </v-data-table>
-            <v-data-footer />
           </v-tabs-window-item>
           <v-tabs-window-item :value="3">
             <h2>Drafts</h2>
@@ -69,7 +110,6 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue'
 import sampleData from './assets/sample-data.json'
-import { ar } from 'vuetify/locale';
 
 const tab = ref(2)
 const items = ref([])
@@ -78,20 +118,33 @@ const error = ref('')
 
 const headers = [
   {
-    align: 'start',
-    key: 'reference',
+    key: 'actions',
     sortable: false,
-    title: 'Shipping Note Reference',
-    minWidth: 200,
+    minWidth: '50',
+    disableSort: true,
   },
-  { key: 'status', title: 'Status' },
-  { key: 'operation.name', title: 'Operation', minWidth: 200 },
-  { key: 'principalName', title: 'Principle Name', minWidth: 200 },
-  { key: 'officeDeparture.name', title: 'Departure Office', minWidth: 200 },
-  { key: 'submittedDate', title: 'Submitted Date' },
-  { key: 'departureDateTime', title: 'Departure Date' },
-  { key: 'estimatedArrivalDate', title: 'Scheduled Date Time' },
+  {
+    key: 'reference',
+    title: 'Shipping Note Reference',
+    minWidth: '240',
+  },
+  { key: 'status', title: 'Status', align: 'center', minWidth: '200', sortable: false },
+  { key: 'operation.name', title: 'Operation',minWidth: '200' },
+  { key: 'principalName', title: 'Principle Name', minWidth: '200' },
+  { key: 'officeDeparture.name', title: 'Departure Office', minWidth: '200' },
+  { key: 'submittedDate', title: 'Submitted Date', minWidth: '200' },
+  { key: 'departureDateTime', title: 'Departure Date', minWidth: '200' },
+  { key: 'estimatedArrivalDate', title: 'Scheduled Date Time', minWidth: '200' },
 ];
+
+const pagination = ref({
+  page: 0,
+  size: 0,
+  totalElements: 0,
+  totalPages: 0,
+})
+
+const shippingNoteReference = ref('')
 
 fetch('https://api.jsonserve.com/L0Yu2N',
   { method: "GET", mode: 'cors', headers: { 'Content-Type': 'application/json', } })
@@ -101,6 +154,12 @@ fetch('https://api.jsonserve.com/L0Yu2N',
   .catch(e => {
     console.error(e)
     items.value = sampleData.content
+    pagination.value = {
+      page: sampleData.pageable.pageNumber,
+      size: sampleData.pageable.pageSize,
+      totalElements: sampleData.totalElements,
+      totalPages: sampleData.totalPages,
+    }
   })
   .finally(() => {
     loading.value = false
@@ -133,5 +192,33 @@ const statusOptions = computed(() => {
   })))
 })
 
+const statusToColorMap = {
+  'PAID': 'green',
+  'ARRIVED': 'green',
+  'EXITED': 'green',
+  'DEPARTED': 'amber',
+  'LATE': 'red'
+}
+
+const getColor = (value: string) => {
+  return statusToColorMap[value as keyof typeof statusToColorMap];
+}
+
+const  formatDate = (dateString: string) => {
+  const date = new Date(dateString);
+
+  const day = String(date.getUTCDate()).padStart(2, '0');
+  const month = String(date.getUTCMonth() + 1).padStart(2, '0'); // Month is 0-indexed
+  const year = date.getUTCFullYear();
+
+  let hours = date.getUTCHours();
+  const minutes = String(date.getUTCMinutes()).padStart(2, '0');
+  const seconds = String(date.getUTCSeconds()).padStart(2, '0');
+  const ampm = hours >= 12 ? 'PM' : 'AM';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+
+  return `${day}/${month}/${year} ${hours}:${minutes}:${seconds} ${ampm}`;
+}
 
 </script>
--- src/components/Appbar.vue
+++ src/components/Appbar.vue
@@ -1,25 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
 
 <template>
-    <v-app-bar :elevation="0">
+    <v-app-bar flat density="compact">
         <template v-slot:prepend>
             <v-app-bar-nav-icon></v-app-bar-nav-icon>
         </template>
 
-        <v-app-bar-title>
-            <v-breadcrumbs :items="['HOME', 'TRANSIT SHIPPING NOTES WORKLIST']"></v-breadcrumbs>
+        <v-app-bar-title class="text-body-1">
+            <v-breadcrumbs density="compact" :items="['HOME', 'TRANSIT SHIPPING NOTES WORKLIST']">
+                <template v-slot:divider>
+                    <v-icon icon="mdi-chevron-right"></v-icon>
+                </template>
+            </v-breadcrumbs>
         </v-app-bar-title>
 
         <template v-slot:append>
-            <v-text-field placeholder="Type here to search" prepend-inner-icon="mdi-magnify" min-width="200"></v-text-field>
+            <v-text-field placeholder="Type here to search" prepend-inner-icon="mdi-magnify" min-width="200"
+                :hide-details="true"></v-text-field>
             <v-btn icon="mdi-history"></v-btn>
 
             <v-btn icon="mdi-source-branch"></v-btn>
 
             <v-btn icon="mdi-bell"></v-btn>
 
-            <v-badge dot>
-                <v-avatar color="surface-variant">JA</v-avatar>
+            <v-badge dot color="primary">
+                <v-avatar color="primary" background-color="white" variant="outlined">
+                    <v-btn rounded="circle" color="primary" variant="flat" min-height="100%" min-width="100%">
+                        <span class="text-white">JA</span>
+                    </v-btn>
+                </v-avatar>
             </v-badge>
         </template>
     </v-app-bar>
